604b93b6001fef6e0d296bcd54c54e4a5fe88b9b
Praggya Rustagi Barretto <praggya@gmail.com>
2014-03-13 11:40:10 -0400

    basic structure

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-window.onload(function() {
+window.onload = (function() {
+  d3.csv("cleaner-data.csv",
+    function(error, rows){
+      var nested_data = d3.nest()
+        .key(function(d) { return d.category } )
+        .entries(rows);
 
+    d3.select("#employment")
+      .selectAll('g')
+      .data(nested_data)
+      .enter()
+        .append('g')
+  });
 });
